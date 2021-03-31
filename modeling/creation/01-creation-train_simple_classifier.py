05b056c7a6db9ae8d037739c93cf1cfe30e6be6f
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-03-31 10:41:48 +0000

    Add initial model training script

---
+++ modeling/creation/01-creation-train_simple_classifier.py
@@ -0,0 +1,43 @@
+import joblib
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+MINIO_DATA_FOLDER = os.getenv("MINIO_DATA_FOLDER")
+
+DIR_DATA_PROCESSED = Path(MINIO_DATA_FOLDER) / "created"
+FILEPATH_DATA = DIR_DATA_PROCESSED / "listings_processed.csv"
+
+DIR_MODEL = Path(MINIO_DATA_FOLDER).parent / "models"
+FILEPATH_MODEL = DIR_MODEL / "simple_classifier.joblib"
+
+
+def main():
+    
+    # Read input data
+    df = pd.read_csv(FILEPATH_DATA, index_col=0).dropna(axis=0)
+
+    # Set up training and testing data
+    FEATURE_NAMES = ['neighbourhood', 'room_type', 'accommodates', 'bathrooms', 'bedrooms']
+    X = df[FEATURE_NAMES]
+    y = df['category']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=1)
+
+    # Train model
+    clf = RandomForestClassifier(n_estimators=120, random_state=0)
+    clf.fit(X_train, y_train)
+
+    # Save model
+    joblib.dump(clf, FILEPATH_MODEL)
+
+
+if __name__ == "__main__":
+    main()    
