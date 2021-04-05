1e96c1087c825a316d43251bc471d114b95b3c46
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-04-05 11:36:51 +0000

    Change mlflow experiment to price-estimator

--- modeling/creation/01-creation-train_simple_classifier.py
+++ modeling/creation/01-creation-train_simple_classifier.py
@@ -2,12 +2,14 @@ import joblib
 import os
 from pathlib import Path
 
+from matplotlib import pyplot as plt
 import mlflow
 from mlflow.exceptions import MlflowException
 from mlflow.tracking import MlflowClient
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split, ParameterGrid
@@ -19,15 +21,16 @@ DIR_DATA_PROCESSED = Path(MINIO_DATA_FOLDER) / "created"
 FILEPATH_DATA = DIR_DATA_PROCESSED / "listings_processed.csv"
 
 DIR_MODEL = Path(MINIO_DATA_FOLDER).parent / "models"
+DIR_TEMP = Path("temp")  # Temporary location to save results before logging to MlFlow
 FILEPATH_MODEL = DIR_MODEL / "simple_classifier.joblib"
 
 MLFLOW_URL = os.getenv("MLFLOW_URL")
-MLFLOW_EXPERIMENT = "airbnb-price-estimator" # os.getenv("BUCKET_NAME")    # "airbnb-price-estimation"  #airbnb-specify-s3-mlflow-artifacts
+MLFLOW_EXPERIMENT = "price-estimator"  # os.getenv("BUCKET_NAME")    # "airbnb-price-estimation"  #airbnb-specify-s3-mlflow-artifacts
 MLFLOW_RUN_NAME = "test-mlflow-exp-creation"
 
 RF_PARAMS = dict(
-    n_estimators = [100], 
-    max_depth = [8],  #4, 6, 8, 12, 16
+    n_estimators = [100], #, 200, 400], 
+    max_depth = [4], #, 6, 8, 12, 16],
     max_features = [0.4, 0.6, 0.8],
     min_samples_split = [2, 4],
     class_weight=["balanced"], 
@@ -37,20 +40,38 @@ RF_PARAMS = dict(
 INCLUDE_AMENITIES = False
         
 
+def plot_confusion_matrix(y_pred: np.ndarray, y_true: np.ndarray, filepath: str, classes=[0, 1, 2, 3]: list, labels=['low', 'mid', 'high', 'lux']: list) -> None:
+    """ 
+    Given arrays for predictions (y_pred) and true values (y_true) of a binary variable, plots a confusion matrix for the classes 
+    with their provided labels and saves it to the location specified in filepath.
+    """
+    c = confusion_matrix(y_true, y_pred)
+    c = c / c.sum(axis=1).reshape(len(classes), 1)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    sns.heatmap(c, annot=True, cmap='BuGn', square=True, fmt='.2f', annot_kws={'size': 10}, cbar=False)
+    plt.xlabel('Predicted', fontsize=16)
+    plt.ylabel('Real', fontsize=16)
+    plt.xticks(ticks=np.arange(.5, len(classes)), labels=labels, rotation=0, fontsize=12)
+    plt.yticks(ticks=np.arange(.5, len(classes)), labels=labels, rotation=0, fontsize=12)
+    
+    plt.savefig(filepath)
+
+
 def main():
     """
     The main function of the script for training a simple classifier.
-    """
+    """    
+    os.mkdir(DIR_TEMP)
     
-    
-    # Temporary code 
+    # Temporary solution for an issue with mlflow artifacts buckets creation:
     client = mlflow.tracking.MlflowClient(tracking_uri=MLFLOW_URL)
     if not MLFLOW_EXPERIMENT in [exp.name for exp in client.list_experiments()]:
         client.create_experiment(MLFLOW_EXPERIMENT, artifact_location="s3://ny-price-estimator/mlflow-artifacts")
 
     mlflow.set_tracking_uri(MLFLOW_URL)
-    mlflow.set_experiment(MLFLOW_EXPERIMENT)
-    
+    mlflow.set_experiment_(MLFLOW_EXPERIMENT)
+
     with mlflow.start_run(run_name=MLFLOW_RUN_NAME):
         
         # Read input data
@@ -88,12 +109,15 @@ def main():
                 metrics['accuracy'] = accuracy_score(y_test, y_pred)
                 metrics['roc_auc'] = roc_auc_score(y_test, y_proba, multi_class='ovr')
                 
+                plot_confusion_matrix(y_pred=y_pred, y_test=y_test, filepath=str(DIR_PATH / "confusion_matrix.png"))
+
                 # Log to MLflow
                 mlflow.log_params(params)
                 mlflow.log_param("amenities", INCLUDE_AMENITIES)
 
                 mlflow.log_metrics(metrics)
                 mlflow.log_artifact(str(FILEPATH_MODEL))
+                mlflow.log_artifacts(str(DIR_PATH))
 
 
 if __name__ == "__main__":
