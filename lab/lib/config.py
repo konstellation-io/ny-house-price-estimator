5995fdbe73450ee258b740d34970ec5947bec0f9
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-21 17:48:01 +0000

    Doc update: module docstring and comment

--- lab/lib/config.py
+++ lab/lib/config.py
@@ -1,3 +1,7 @@
+"""
+Reusable code for configuration loading
+"""
+
 import configparser
 
 
@@ -5,6 +9,9 @@ def load_config(path_config):
     """
     Loads a configuration from .ini file and returns as configparser
     """
+    # If config path was obtained from environment variable, in case of the env variable not existing, it will
+    # fail silently returning None. In that case, Configparser does not refuse reading from None but simply
+    # loads nothing. Instead, better fail early:
     if path_config is None:
         raise FileNotFoundError(f"Could not find config at path: {str(path_config)}")
 
