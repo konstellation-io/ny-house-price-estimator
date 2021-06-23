356039e5765cf985f4a03465487a91fd75c14bba
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-23 09:00:00 +0000

    Add module docstring

--- lab/lib/data_processing.py
+++ lab/lib/data_processing.py
@@ -1,3 +1,7 @@
+"""
+Reuasable functions for data processing, used by analysis notebooks as well as process code
+"""
+
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -8,9 +12,9 @@ def preprocess_target_variable(df: DataFrame) -> DataFrame:
     Converts the target variable column from string to numeric ('$100.00' -> 100)
     and creates price categories corresponding to low/mid/high/luxury properties.
     """
-    df['price'] = df['price'].str.extract(r"(\d+).")
-    df['price'] = df['price'].astype(int)
-    df['category'] = pd.cut(df['price'], bins=[10, 90, 180, 400, np.inf], labels=[0, 1, 2, 3])
+    df["price"] = df["price"].str.extract(r"(\d+).")
+    df["price"] = df["price"].astype(int)
+    df["category"] = pd.cut(df["price"], bins=[10, 90, 180, 400, np.inf], labels=[0, 1, 2, 3])
     return df
 
 
@@ -20,24 +24,23 @@ def preprocess_amenities_column(df: DataFrame) -> DataFrame:
     ('Heating', 'TV', 'Internet', 'Wifi' etc.), extracts the individual amenities
     as one-hot encoded features.
     """
-    df['TV'] = df['amenities'].str.contains('TV')
-    df['TV'] = df['TV'].astype(int)
-    df['Internet'] = df['amenities'].str.contains('Internet')
-    df['Internet'] = df['Internet'].astype(int)
-    df['Air_conditioning'] = df['amenities'].str.contains('Air conditioning')
-    df['Air_conditioning'] = df['Air_conditioning'].astype(int)
-    df['Kitchen'] = df['amenities'].str.contains('Kitchen')
-    df['Kitchen'] = df['Kitchen'].astype(int)
-    df['Heating'] = df['amenities'].str.contains('Heating')
-    df['Heating'] = df['Heating'].astype(int)
-    df['Wifi'] = df['amenities'].str.contains('Wifi')
-    df['Wifi'] = df['Wifi'].astype(int)
-    df['Elevator'] = df['amenities'].str.contains('Elevator')
-    df['Elevator'] = df['Elevator'].astype(int)
-    df['Breakfast'] = df['amenities'].str.contains('Breakfast')
-    df['Breakfast'] = df['Breakfast'].astype(int)
+    df["TV"] = df["amenities"].str.contains("TV")
+    df["TV"] = df["TV"].astype(int)
+    df["Internet"] = df["amenities"].str.contains("Internet")
+    df["Internet"] = df["Internet"].astype(int)
+    df["Air_conditioning"] = df["amenities"].str.contains("Air conditioning")
+    df["Air_conditioning"] = df["Air_conditioning"].astype(int)
+    df["Kitchen"] = df["amenities"].str.contains("Kitchen")
+    df["Kitchen"] = df["Kitchen"].astype(int)
+    df["Heating"] = df["amenities"].str.contains("Heating")
+    df["Heating"] = df["Heating"].astype(int)
+    df["Wifi"] = df["amenities"].str.contains("Wifi")
+    df["Wifi"] = df["Wifi"].astype(int)
+    df["Elevator"] = df["amenities"].str.contains("Elevator")
+    df["Elevator"] = df["Elevator"].astype(int)
+    df["Breakfast"] = df["amenities"].str.contains("Breakfast")
+    df["Breakfast"] = df["Breakfast"].astype(int)
 
-    df.drop('amenities', axis=1, inplace=True)
-    
+    df.drop("amenities", axis=1, inplace=True)
+
     return df
-
