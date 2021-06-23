7560f4073f68cae9e800c9e641f9d3886d981521
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-23 09:04:26 +0000

    Minor: typing, docstring, better variable name

--- lab/lib/viz.py
+++ lab/lib/viz.py
@@ -1,3 +1,9 @@
+"""
+Reusable visualization code
+"""
+
+from typing import Union
+
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
@@ -8,18 +14,18 @@ def plot_confusion_matrix(
     y_pred: np.ndarray,
     y_true: np.ndarray,
     filepath: str,
-    classes: list = [0, 1, 2, 3],
-    labels: list = ["low", "mid", "high", "lux"],
+    classes: Union[list, tuple] = (0, 1, 2, 3),
+    labels: Union[list, tuple] = ("low", "mid", "high", "lux"),
 ) -> None:
     """
     Given arrays for predictions (y_pred) and true values (y_true) of a binary variable, plots a confusion matrix for the classes
     with their provided labels and saves it to the location specified in filepath.
     """
-    c = confusion_matrix(y_true, y_pred)
-    c = c / c.sum(axis=1).reshape(len(classes), 1)
+    conf_matrix = confusion_matrix(y_true, y_pred)
+    conf_matrix = conf_matrix / conf_matrix.sum(axis=1).reshape(len(classes), 1)
 
     fig, ax = plt.subplots(figsize=(10, 10))
-    sns.heatmap(c, annot=True, cmap="BuGn", square=True, fmt=".2f", annot_kws={"size": 10}, cbar=False)
+    sns.heatmap(conf_matrix, annot=True, cmap="BuGn", square=True, fmt=".2f", annot_kws={"size": 10}, cbar=False)
     plt.xlabel("Predicted", fontsize=16)
     plt.ylabel("Real", fontsize=16)
     plt.xticks(ticks=np.arange(0.5, len(classes)), labels=labels, rotation=0, fontsize=12)
